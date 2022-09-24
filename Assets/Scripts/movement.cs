f019377a2108c974ab3595e5dbc889286f9a21a2
gnarley2 <marley.proud@gmail.com>
2022-09-24 22:02:01 +1000

    setting up for movement

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+public GameObject littleDude;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+    //float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+    //timeFraction = Mathf.Pow(timeFraction, 3);
+littleDude.transform.position=new Vector3();
+        
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
